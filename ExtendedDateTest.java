ab5c8f834af12a724db8644c3a214e69ad27962d
Larry Vail <lvail@.olivet.edu>
2017-11-10 07:40:14 -0600

    Added JUnit tests for ExtendedDate class.
    Signed-off-by: Larry Vail <lvail@.olivet.edu>

--- ExtendedDateTest.java
+++ ExtendedDateTest.java
@@ -1,3 +1,13 @@
+/**
+CSIS 251 Principles of Programming I<br>
+Project (pr4) Date and ExtendedDate classes<br>
+<br>
+ExtendedDateTest.java - provided JUnit tests for ExtendedDate class<br>
+Author by Brittnay Schmid<br>
+Modified by Dr. Larry Vail<br>
+Date November 9, 2017<br>
+*/
+
 import org.junit.Assert;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -7,19 +17,63 @@ import org.junit.Test;
 public class ExtendedDateTest {
 
 
-   /** Fixture initialization (common initialization
-    *  for all tests). **/
    @Before public void setUp() {
    }
 
-
-   @Test 
-   public void defaultTest() {
+   @Test
+   public void defaultContructorTest(){
       ExtendedDate d = new ExtendedDate();
-      d.setDate(3, 24, 2015);
-      Assert.assertEquals("March 24, 2015", d.toString());
-      
-      ExtendedDate d2 = new ExtendedDate(d);
-      Assert.assertEquals("March 24, 2015", d2.toString());
+      Assert.assertEquals("January 1, 1500", d.toString());      
    }
+   
+   @Test
+   public void otherContructorTest(){
+      ExtendedDate d = new ExtendedDate(3, 24, 2015);
+      Assert.assertEquals("March 24, 2015", d.toString());      
+      d = new ExtendedDate(2, 28, 2017);
+      Assert.assertEquals("February 28, 2017", d.toString());      
+   }
+   
+   @Test (expected = IllegalArgumentException.class)
+   public void otherConstructorIllegalArgumentExceptionTest() {
+      ExtendedDate d = new ExtendedDate(2, 29, 2017);
+      Assert.fail("Expected IllegalArgumentException for 2/29/2017, but did not.");
+   }
+   
+   @Test
+   public void copyContructorTest(){
+      ExtendedDate d = new ExtendedDate(3, 24, 2015);
+      ExtendedDate d2 = new ExtendedDate(d);
+      Assert.assertEquals("March 24, 2015", d2.toString());      
+   }
+  
+   @Test
+   public void toStringLongTest(){
+      ExtendedDate d = new ExtendedDate(3, 24, 2015);
+      Assert.assertEquals("March 24, 2015", d.toStringLong());      
+      d = new ExtendedDate(2, 28, 2017);
+      Assert.assertEquals("February 28, 2017", d.toString());      
+   }
+
+   @Test
+   public void toStringShortTest(){
+      ExtendedDate d = new ExtendedDate(3, 24, 2015);
+      Assert.assertEquals("3/24/2015", d.toStringShort());      
+      d = new ExtendedDate(2, 28, 2017);
+      Assert.assertEquals("2/28/2017", d.toStringShort());      
+   }
+   @Test
+   public void toStringMonthYearTest(){
+      ExtendedDate d = new ExtendedDate(3, 24, 2015);
+      Assert.assertEquals("March 2015", d.toStringMonthYear());      
+      d = new ExtendedDate(2, 28, 2017);
+      Assert.assertEquals("February 2017", d.toStringMonthYear());      
+   }
+   @Test
+   public void toStringTest(){
+      ExtendedDate d = new ExtendedDate(3, 24, 2015);
+      Assert.assertEquals("March 24, 2015", d.toString());      
+      d = new ExtendedDate(2, 28, 2017);
+      Assert.assertEquals("February 28, 2017", d.toString());      
+   }          
 }
